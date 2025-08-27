594dd5dc7094902dc17a5f49627fa2d8917767db
Nick Vincent <31055081+nick-vincent@users.noreply.github.com>
2025-08-26 18:48:26 -0700

    fix: favicon (#23)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
